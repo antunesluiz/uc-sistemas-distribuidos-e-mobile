277d2205e7a71bf09778a01fd03beed7c8721e3b
Luiz Antunes <antunesluiz27@gmail.com>
2021-09-17 21:59:56 -0300

    fix valor de compra

--- Consumo api 02/exercicio01.py
+++ Consumo api 02/exercicio01.py
@@ -15,8 +15,8 @@ response = requests.get(url, params=params)
 if (response):
     currencies = response.json()['currencies']
 
-    usd = currencies['USD']['sell']
-    eur = currencies['EUR']['sell']
+    usd = currencies['USD']['buy']
+    eur = currencies['EUR']['buy']
     date = datetime.now()
 
     params = [(
@@ -26,6 +26,14 @@ if (response):
     conn = sqlite3.connect('bdcotacoes.db', timeout=10)
 
     cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS moedas (
+            Data DATE NOT NULL PRIMARY KEY,
+            Dolar FLOAT NOT NULL,
+            Euro FLOAT NOT NULL
+        );
+    """)
 
     cursor.executemany("""
         INSERT INTO moedas (Data, Dolar, Euro)
